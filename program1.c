15c32d9bad190f041f96b43b0dee8d181ed02614
oblivion94 <u11302257@o365.ttu.edu.tw>
2025-04-01 18:54:06 +0800

    write the homework-1 file

---
+++ program1.c
@@ -0,0 +1,65 @@
+#include<stdio.h>
+#include<stdlib.h>
+int main(void)
+{
+    /*步驟1 顯示個人風格的畫面*/ 
+	int i;
+	for(i=0;i<4;i++)
+	 {
+		 printf("********************\n");
+		 printf("*\n");
+		 printf("程式設計作業一\n"); 
+		 printf("*\n");
+		 printf("********************\n");
+	 } 
+	printf("\n");
+	printf("按Enter鍵繼續...\n");
+	getchar();
+	fflush(stdin);
+	system("cls");
+	
+	/*步驟2 驗證密碼*/ 
+	int pass=2024,inpass;
+	
+	printf("請輸入4位數字密碼:\n");
+	scanf("%d",&inpass);
+	
+	if(inpass==pass)
+	{
+		printf("歡迎!\n");
+		/*步驟3 顯示主選單*/
+		system("cls");
+		printf("--------------------------\n");
+		printf("| 'A'...'Z' : Uppercase  |\n");
+		printf("| 'a'...'z' : Lowercase  |\n");
+		printf("| '0'...'9' : Digit      |\n");
+		printf("| Otherwise : E1B57陳逸安|\n");
+		printf("--------------------------\n");
+		/*步驟4 處理字元輸入*/
+		char inch;
+		printf("請輸入一個字元\n");
+		scanf(" %c",&inch);
+		if(inch>='A' && inch<='Z')
+		 printf("Uppercase\n");
+		else if(inch>='a' && inch<='z')
+	  	 printf("Lowercase\n");
+		else if(inch>='0' && inch<='9')
+	  	 printf("Digit\n");
+	  	else
+	  	 printf("E1B57陳逸安\n");
+
+	}
+	else
+	{
+		printf("密碼錯誤\a\n");
+	}
+	system("pause");
+	return 0;
+}
+//心得
+//這次作業讓我更熟悉輸入指令還有一些形態上的運用
+//但最重要的還是學習到了github使用的基本知識 
+
+
+
+

2ab7fd34253e2ba36d488030138665421da2a566
oblivion94 <u11302257@o365.ttu.edu.tw>
2025-04-28 01:45:08 +0800

    完成主選單，並輸出*做成的直角三角形

--- hw2.c
+++ hw2.c
@@ -1,10 +1,12 @@
 #include<stdio.h>
 #include<stdlib.h>
+#include<unistd.h>
 #define password 2025
 #define maxpasswordtry 3
+void funcA(void);
 int main(void)
-{
-	//輸出至少20行個人風格的畫面 
+{ 
+	//步驟一  輸出至少20行個人風格的畫面 
 	printf("************************************\n");
 	printf("* *\n");
 	printf("* *\n");
@@ -34,6 +36,31 @@ int main(void)
 		if(inputpass==password)
 		 {
 			printf("密碼正確!\n");
+			//步驟二 螢幕出現主選單 
+			usleep(500000);
+			system("cls");
+			printf("*****************主選單*****************\n");
+			printf("| a. 畫出直角三角形\n");
+			printf("| b. 顯示乘法表\n");
+			printf("| c. 結束\n");
+			printf("*****************主選單*****************\n");
+			printf("\n");
+			//步驟三 直角三角形
+			char choice;
+			printf("請輸入您的選擇 (a/b/c): \n");
+			scanf(" %c",&choice);
+			switch(choice)
+			{
+				case 'A':
+				case 'a':
+				 funcA(); 
+				 break; 
+
+			}
+			
+			
+			
+			
 			
 			
 			
@@ -62,4 +89,31 @@ int main(void)
 	return 0;
 }
 
-//修正前兩次密碼錯誤，第三次正確時，不會顯示"密碼正確"的問題 
+void funcA(void)
+{
+	int i,j,k;
+	char input;
+	system("cls");
+	printf("請輸入一個 'a' 到 'n' 的字元\n");
+	scanf(" %c",&input);
+	int height=(input-'a'+1);
+	if((input>='a')&&(input<='n'))
+	{
+		for(i=1;i<=height;i++)
+		{
+		  for(j=1;j<=(height-i);j++)
+		  {
+		  printf(" ");
+		  }
+		  
+		  for(j=1;j<=i;j++)
+		  {
+		  printf("*");
+		  }
+		  printf("\n");
+		}
+		 
+		
+	}
+}
+//完成主選單，並輸出*做成的直角三角形 
